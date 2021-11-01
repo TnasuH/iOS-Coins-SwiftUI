8faf10ad026ea7eb8a3111523235cfec3f67a566
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-11-02 01:28:25 +0300

    project created and color extension added for theme.

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Tarık Nasuhoğlu on 2.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
