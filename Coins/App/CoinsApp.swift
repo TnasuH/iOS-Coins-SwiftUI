24cc4cc2b71154843d68b7a71597fa58cba85906
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-11-02 03:11:52 +0300

    homeview created with navigationheader

--- Coins/App/CoinsApp.swift
+++ Coins/App/CoinsApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CoinsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
---
+++ Coins/Core/Home/Views/HomeView.swift
@@ -0,0 +1,66 @@
+//
+//  HomeView.swift
+//  Coins
+//
+//  Created by Tarık Nasuhoğlu on 2.11.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var isPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            //background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            //contents
+            VStack {
+                headerView
+                Spacer(minLength: 0)
+            }
+            
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var headerView: some View {
+        HStack {
+            CircleButtonView(iconName: isPortfolio ? "plus":"info")
+                .animation(.none, value: isPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $isPortfolio)
+                )
+            Spacer()
+            Text(isPortfolio ? "Portfolio": "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(.theme.accent)
+                .animation(.none, value: isPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: isPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        isPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
