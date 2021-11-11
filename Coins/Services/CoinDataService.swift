c166af3187967e0214b44406f7f3df9fdc2e23a3
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-11-11 04:42:22 +0300

    networkingManager created. and API call integrated..

---
+++ Coins/Services/CoinDataService.swift
@@ -0,0 +1,30 @@
+//
+//  CoinDataService.swift
+//  Coins
+//
+//  Created by Tarık Nasuhoğlu on 11.11.2021.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins = [CoinModel]()
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins(){
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
