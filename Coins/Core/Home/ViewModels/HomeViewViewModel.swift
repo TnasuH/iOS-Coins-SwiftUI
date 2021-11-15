a3a89ef48b602ab75e74f05ab051821a4b30797c
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-11-15 15:27:38 +0300

    integrated searchText via combine

--- Coins/Core/Home/ViewModels/HomeViewViewModel.swift
+++ Coins/Core/Home/ViewModels/HomeViewViewModel.swift
@@ -22,10 +22,25 @@ final class HomeViewViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
     }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return coins }
+        let lowerText = text.lowercased()
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowerText) ||
+            coin.symbol.lowercased().contains(lowerText) ||
+            coin.id.lowercased().contains(lowerText)
+        }
+    }
+    
 }
